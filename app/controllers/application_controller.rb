a6a166322e8144ee377e2c803e45b376406d3709
Carlo Fontanos <carl.fontanos@gmail.com>
2016-10-10 16:57:09 +0800

    Made the current user global.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+	protect_from_forgery with: :exception
+	
+	# if they're logged in - save their user object to a variable called @current_user
+	def current_user
+		@current_user ||= User.find(session[:user_id]) if session[:user_id]
+	end
+	
+	# Make @current_user accessible in our view files.
+	helper_method :current_user
+	
+	# Send user back to the login page if they are not logged-in
+	def authorize
+		redirect_to '/login' unless current_user
+	end
 end
